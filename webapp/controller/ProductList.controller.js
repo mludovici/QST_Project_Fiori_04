f9f638f1489f64698cc619f3ca040d26cac76387
Lynn Keller <lynn.keller@adesso-orange.com>
2023-02-01 10:14:15 +0000

    detail List nach supplier

--- webapp/controller/ProductList.controller.js
+++ webapp/controller/ProductList.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/core/Fragment"
     ],
-    function(Controller) {
+    function(Controller, Fragment) {
       "use strict";
   
       return Controller.extend("qst4.controller.ProductList", {
@@ -12,10 +13,29 @@ sap.ui.define(
         },       
         onItemPressed: function (oEvent) {
             var oItem = oEvent.getSource();
-            var sPath= oItem.getBindingContext().getPath(); 
-            var sIndex = sPath.substr(sPath.lastIndexOf("/") + 1);
+            var sPath= oItem.getBindingContext().getPath(); // /Products(0)
+            //var sIndex = sPath.substr(sPath.lastIndexOf("/") + 1);
             var oDetail = this._oParent.getView().byId("detail");
-            oDetail.bindElement(sPath);
+            oDetail.bindElement({
+                path: sPath
+            });
+            var oDetailList = this._oParent.getView().byId("detailFragment--detailList--list");
+
+            var oModel = this._oParent.getOwnerComponent().getModel();
+                oModel.read(sPath + "/Supplier", {
+                    success: function(oData){
+                        var sSupplier = oData.Name;
+                        console.log("oData", sSupplier);
+                        var oFilter = new sap.ui.model.Filter(
+                          "Supplier/Name",
+                          sap.ui.model.FilterOperator.EQ,
+                          sSupplier
+                        );
+                        oDetailList.getBinding("items").filter([oFilter]);
+                    }
+                });
+
+
         }
       });
     }
