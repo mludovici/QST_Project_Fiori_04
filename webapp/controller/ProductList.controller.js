ad7ecb9c9f1121a2b06e949ae004fa524d390cf3
Marc Ludovici <marc.ludovici@adesso-orange.com>
2023-02-01 09:58:11 +0000

    commit filter test marc

--- webapp/controller/ProductList.controller.js
+++ webapp/controller/ProductList.controller.js
@@ -16,6 +16,40 @@ sap.ui.define(
             var sIndex = sPath.substr(sPath.lastIndexOf("/") + 1);
             var oDetail = this._oParent.getView().byId("detail");
             oDetail.bindElement(sPath);
+        },
+        onFilter: function(oEvent) {
+          var oItem = oEvent.getSource();
+          var sPath= oItem.getBindingContext().getPath(); 
+          console.log("sPath:", sPath);
+          // var sIndex = sPath.substr(sPath.lastIndexOf("/") + 1);
+          //   var oDetail = this._oParent.getView().byId("detail");
+          //   oDetail.bindElement(sPath);
+        },
+        onSelectionChange: function(oEvent) {
+          // var oITT = oEvent.getSelectedItem();
+          // console.log(oITT);
+          debugger;
+          var sItem = oEvent.getParameter("selectedItem");
+          console.log(sItem);
+          var oItem = oEvent.getSource();
+          console.log(oItem);
+          var sPath = oItem.getBindingContext().getPath();
+          console.log("path:", sPath);
+
+          // var pars = oEvent.getParameters("selectedItem");
+          // console.log(oItem);
+          // console.log(oItem.getKeys("items"));
+          // var item = oItem.getBinding("items");
+          // console.log("items:", item);
+          // var sPath= oItem.getBindingPath(); 
+          // console.log("sPath:", sPath);
+
+          // var oInput = this.getView().byId("input0");
+          // var oList = this.getView().byId("__table0");
+          // var oListValue = oInput.getValue();
+          // var oFilter = new sap.ui.model.Filter("oscYear", FilterOperator.GE, oListValue);
+          // var oBinding = oList.getBinding("items");
+          // oBinding.filter(oFilter);
         }
       });
     }
